637323bcc795b54a46b317c9d7913c2e160e5d34
Shiuan Kai <t105360045@ntut.org.tw>
2018-09-23 03:36:12 +0800

    First commit

---
+++ Sample10/src/Sample10.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Sample10
+{
+	
+	public static void main(String[] args)throws IOException
+	{
+		System.out.println("請輸入一個整數");
+		
+		BufferedReader br1= new BufferedReader(new InputStreamReader(System.in));
+		
+		String str1=br1.readLine();
+		
+		int num
+		
+	}
+}
