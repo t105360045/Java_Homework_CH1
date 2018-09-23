3d1cb882e9c40ec7c4e96e26670125c3f67be12c
Shiuan Kai <t105360045@ntut.org.tw>
2018-09-23 14:43:51 +0800

    作業完成

--- Sample10/src/Sample10.java
+++ Sample10/src/Sample10.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
+//import java.io.*;                 //可以用這個
 public class Sample10
 {
 	
@@ -8,11 +9,20 @@ public class Sample10
 	{
 		System.out.println("請輸入一個整數");
 		
-		BufferedReader br1= new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br1 = new BufferedReader(new InputStreamReader (System.in));
 		
-		String str1=br1.readLine();
+		String str1 = br1.readLine();
 		
-		int num
+		int num = Integer.parseInt(str1);
+		System.out.println("您輸入的數字是:"+num);
+		System.out.println("請輸入字串");
+		
+		BufferedReader br2 = new BufferedReader(new InputStreamReader (System.in));
+		
+		String str2 = br2.readLine();
+		
+		System.out.println("剛剛輸入的字串是:"+str2);
+		
 		
 	}
 }
